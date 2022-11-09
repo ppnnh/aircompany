25923ba2462d3f25f701bf1848bd79a158a7c30d
lerochka <76012407+ppnnh@users.noreply.github.com>
2022-11-09 20:08:36 +0300

    Update classificationLevel.js

--- JS/models/classificationLevel.js
+++ JS/models/classificationLevel.js
@@ -2,7 +2,7 @@ const ClassificationLevel  = {
     UNCLASSIFIED: 'Unclassified',
     CONFIDENTIAL: 'Confidential',
     SECRET: 'Secret',
-    TOP_SECRET: 'Top_secret'
+    TOP_SECRET: 'TopSecret'
 };
 
 module.exports =  ClassificationLevel;
