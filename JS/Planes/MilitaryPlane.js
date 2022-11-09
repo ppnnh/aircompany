b0ed527efabd05ae8646d966997e96feed3ef440
lerochka <76012407+ppnnh@users.noreply.github.com>
2022-11-09 20:06:36 +0300

    Update MilitaryPlane.js

--- JS/Planes/MilitaryPlane.js
+++ JS/Planes/MilitaryPlane.js
@@ -5,11 +5,14 @@ class MilitaryPlane extends Plane {
     constructor(model, maxSpeed, maxFlightDistance, maxLoadCapacity, militaryType) {
         super(model, maxSpeed, maxFlightDistance, maxLoadCapacity);
         this.militaryType = militaryType;
-
     }
 
-    getMilitaryType() {
+    get MilitaryType() {
         return this.militaryType;
+    }
+
+    set MilitaryType(value){
+        this.militaryType=value;
     }
 }
 
