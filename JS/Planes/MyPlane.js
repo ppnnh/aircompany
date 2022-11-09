0efa88365991baaf153ee9caef83b9c3be41df8d
lerochka <76012407+ppnnh@users.noreply.github.com>
2022-11-09 20:06:54 +0300

    Update MyPlane.js

--- JS/Planes/MyPlane.js
+++ JS/Planes/MyPlane.js
@@ -4,8 +4,5 @@ class MyPlane  extends Plane {
 
     constructor(model, maxSpeed, maxFlightDistance, maxLoadCapacity) {
         super(model, maxSpeed, maxFlightDistance, maxLoadCapacity);
-
     }
 }
-
-//TODO implement it later
